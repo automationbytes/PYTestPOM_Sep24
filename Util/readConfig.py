eddf75f45238e786e8c9e47e613c2a8415177877
automationbytes <automationbytes@gmail.com>
2024-09-29 19:59:37 +0530

    Added POM

--- Util/readConfig.py
+++ Util/readConfig.py
@@ -1,12 +1,15 @@
 import configparser
-
+import os
 config = configparser.RawConfigParser()
-config.read("../Config/config.ini")
+config.read("./Config/config.ini")
+
+
 
 class readConfig:
 
     @staticmethod
     def getConfig(header,key):
+        print(os.path.abspath("./Config/config.ini"))
         print(config.get(header,key))
         return config.get(header,key)
 
